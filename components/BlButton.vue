593cb1ecf17ab5bbb5c7515eb15bacdb9010925c
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-02-14 21:51:40 +0700

    add button component

---
+++ components/BlButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="bl-button">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.bl-button {
+  width: 249px;
+  height: 48px;
+  border-radius: 24px;
+  box-shadow: 0 4px 6px 0 #cacaca;
+  background-color: #ea5164;
+  border: none;
+  color: #fff;
+  font-weight: bold;
+}
+</style>
