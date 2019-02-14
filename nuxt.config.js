c25b5325bd93553da6a452a638b37486b4f55121
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-02-14 15:45:17 +0700

    add vuecarousel plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+  plugins: [{ src: './plugins/vue_carousel.js', ssr: false }],
 
   /*
   ** Nuxt.js modules
