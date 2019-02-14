b94a7450bc973eb517eec0863dc238b994456364
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-02-14 15:45:31 +0700

    render from data

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -1,28 +1,18 @@
 <template>
   <section class="timeline">
     <ul>
-      <li class="timeline-item">
+      <li v-for="(item, m) in timelineItems" :key="m" class="timeline-item">
         <div class="nav-column">
           <glowing-button>1</glowing-button>
         </div>
         <div class="content-column">
-          content
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="nav-column">
-          <glowing-button>2</glowing-button>
-        </div>
-        <div class="content-column">
-          content
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="nav-column">
-          <glowing-button>3</glowing-button>
-        </div>
-        <div class="content-column">
-          content
+          <no-ssr>
+            <carousel :per-page="1">
+              <slide v-for="(slideText, n) in item.slides" :key="n">
+                {{ slideText }}
+              </slide>
+            </carousel>
+          </no-ssr>
         </div>
       </li>
     </ul>
@@ -35,9 +25,44 @@ import GlowingButton from './GlowingButton'
 export default {
   components: {
     GlowingButton
+  },
+  data() {
+    return {
+      timelineItems: [
+        {
+          slides: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe libero dolore aliquid, debitis omnis neque magnam pariatur, aspernatur explicabo obcaecati reiciendis? Eaque optio dolorum, assumenda culpa quaerat ex quia qui!',
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore atque repellendus voluptates voluptatem odio placeat sunt quisquam culpa odit iure. Fugit, dolores recusandae! Soluta suscipit distinctio corporis enim ab consectetur.'
+          ]
+        },
+        {
+          slides: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe libero dolore aliquid, debitis omnis neque magnam pariatur, aspernatur explicabo obcaecati reiciendis? Eaque optio dolorum, assumenda culpa quaerat ex quia qui!',
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore atque repellendus voluptates voluptatem odio placeat sunt quisquam culpa odit iure. Fugit, dolores recusandae! Soluta suscipit distinctio corporis enim ab consectetur.'
+          ]
+        },
+        {
+          slides: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe libero dolore aliquid, debitis omnis neque magnam pariatur, aspernatur explicabo obcaecati reiciendis? Eaque optio dolorum, assumenda culpa quaerat ex quia qui!',
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore atque repellendus voluptates voluptatem odio placeat sunt quisquam culpa odit iure. Fugit, dolores recusandae! Soluta suscipit distinctio corporis enim ab consectetur.'
+          ]
+        }
+      ]
+    }
   }
 }
 </script>
+
+<style>
+.VueCarousel {
+  height: calc((100vh / 3) - 60px);
+}
+
+.VueCarousel-pagination {
+  text-align: left !important;
+}
+</style>
+
 
 <style scoped lang="scss">
 .timeline {
@@ -47,7 +72,7 @@ export default {
 .timeline-item {
   position: relative;
   padding-left: 140px;
-  height: calc((100vh / 3) - 50px);
+  height: calc((100vh / 3) - 30px);
 
   .nav-column {
     position: absolute;
