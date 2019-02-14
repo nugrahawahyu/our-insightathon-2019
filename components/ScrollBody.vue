083c3c15ecabbc16c0df688206f3f1665d02c397
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-02-14 07:23:37 +0700

    add scrollbody component

---
+++ components/ScrollBody.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="scroll-body" :style="`height: ${height};`">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    height: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,75 @@
 <template>
-  <main>
-    <section class="section">
-      <div class="container">
-        <plain-section>
-          intro
-        </plain-section>
-      </div>
-    </section>
-    <section class="section">
-      <div class="container">
-        <interactive-section>
-          content
-        </interactive-section>
-      </div>
-    </section>
-    <section class="section">
-      <div class="container">
-        <plain-section>
-          outro
-        </plain-section>
-      </div>
-    </section>
+  <main ref="scrollBody" style="height: 100vh; overflow-y: scroll;">
+    <scroll-body height="4468px">
+      <section class="section" style="background: red; color: white;">
+        <div class="container">
+          <plain-section>
+            intro
+          </plain-section>
+        </div>
+      </section>
+      <section class="section" style="background: green; color: white;">
+        <div class="container">
+          <interactive-section>
+            content
+          </interactive-section>
+        </div>
+      </section>
+      <section class="section" style="background: blue; color: white;">
+        <div class="container">
+          <plain-section>
+            outro
+          </plain-section>
+        </div>
+      </section>
+    </scroll-body>
   </main>
 </template>
 
 <script>
+/* eslint no-console: ["error", { allow: ["log"] }] */
+
 import PlainSection from '../components/PlainSection.vue'
+import ScrollBody from '../components/ScrollBody.vue'
 import InteractiveSection from '../components/InteractiveSection.vue'
 
 export default {
   components: {
+    ScrollBody,
     PlainSection,
     InteractiveSection
+  },
+  data() {
+    return {
+      scrollTop: null
+    }
+  },
+  watch: {
+    scrollTop() {
+      this.updateScene()
+    }
+  },
+  mounted() {
+    this.$refs.scrollBody.addEventListener('scroll', e => {
+      this.updateScroll(e)
+    })
+  },
+  methods: {
+    updateScroll($event) {
+      this.$set(this, 'scrollTop', $event.target.scrollTop)
+    },
+    updateScene() {
+      console.log(this.scrollTop)
+    }
   }
 }
 </script>
 
 <style>
+html {
+  overflow: hidden;
+}
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
