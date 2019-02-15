c0ac2736e4317586243046b91dc637a3527458b0
Maulana SH <maulana.sholihin@bukalapak.com>
2019-02-15 20:21:53 +0700

    content page 4

--- pages/timelineItems.js
+++ pages/timelineItems.js
@@ -7,40 +7,43 @@ export default [
     slides: [
       {
         text:
-          'Sebelum Ramadhan, transaksi pembelian listrik di Kosan Bukalapak selama bulan April sebanding dengan penggunaan listrik 15.735 rumah selama sebulan.',
-        icon: '/images/ic_listrikprabayar1.png'
-      },
-      {
-        text:
-          'Pembayaran airnya bisa mencapai 2146 transaksi, atau bisa mencukupi kebutuhan rumah tangga untuk 1,8 tahun.',
+          'Pembayaran airnya bisa mencapai <strong>2.146 transaksi</strong>, atau bisa mencukupi kebutuhan rumah tangga selama <strong>22 bulan</strong>.',
         icon: '/images/ic_pdam1.png'
       },
       {
         text:
-          'Internet yang dipakai di kosan Bukalapak bisa digunakan untuk menonton 528.202 video kultum ustadz versi HD (sst persiapan spiritual menyambut ramadhan nih).',
+          'Transaksi untuk pulsa di Kosan Bukalapak sebelum Ramadhan 2018 cukup untuk dipakai menelepon <strong>1.136.730 orang</strong> selama <strong>2 menit</strong>.',
+        icon: '/images/ic_pulsaprabayar1.png'
+      },
+      {
+        text:
+          'Paket internet yang dibeli di Bukalapak bisa dipakai nonton <strong>528.202 video</strong> konten religi dengan kualitas HD. (Ssst….persiapan spiritual menyambut Ramadhan nih!)',
         icon: '/images/ic_paketinternet1.png'
       },
       {
         text:
-          'Transaksi untuk pulsa di kosan Bukalapak sebelum Ramadhan tahun lalu cukup untuk dipakai menelepon 1.136.730 orang selama 2 menit.',
-        icon: '/images/ic_pulsaprabayar1.png'
+          'Sebelum Ramadhan, pembayaran tagihan listrik di Kosan Bukalapak Jakarta sebanding dengan penggunaan listrik untuk <strong>15.735 rumah selama sebulan.</strong>',
+        icon: '/images/ic_listrikprabayar1.png'
       }
     ],
     transactions: {
       virtualProducts: [
         {
           label: '<img src="/images/ic_air_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_air_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_air_disable-min.png" alt="">',
           data: 0.5981709968
         },
         {
           label: '<img src="/images/ic_pulsa_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_pulsa_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_pulsa_disable-min.png" alt="">',
           data: 0.4513497908
         },
         {
           label: '<img src="/images/ic_data_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_data_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_data_disable-min.png" alt="">',
           data: 0.7383487345
         },
         {
@@ -61,37 +64,37 @@ export default [
     slides: [
       {
         text:
-          'Selama bulan puasa, transaksi di Bukalapak bisa menyalakan listrik ke 37.415 rumah selama sebulan atau 1.3x lebih banyak dari bulan sebelumnya.',
-        icon: '/images/ic_listrikprabayar1.png'
-      },
-      {
-        text:
-          'Penggunaan air juga mengalami peningkatan. Kalau dihitung-hitung, air yang dipakai bisa untuk wudhu 231.741 kali atau setara 126.9 tahun sholat…',
+          'Penggunaan air juga mengalami peningkatan. Kalau dihitung-hitung, air yang dipakai bisa untuk wudhu sebanyak <strong>231.741 kali</strong> atau setara dengan shalat terus menerus selama <strong>127 tahun</strong>.',
         icon: '/images/ic_pdam1.png'
       },
       {
         text:
-          'Pembelian pulsa selama bulan Ramadhan bisa menambah 520.103 orang lagi yang ditelepon dari bulan lalu lho. Makin banyak ya orang yang bisa ditanyain kabarnya...',
+          'Pembelian pulsa selama bulan Ramadhan bisa menambah <strong>520.103 orang</strong> lagi yang ditelepon dibandingkan bulan lalu lho. Makin banyak ya orang yang bisa ditanyain kabarnya...',
         icon: '/images/ic_pulsaprabayar1.png'
       },
       {
         text:
-          'Internet yang dibeli di Bukalapak di bulan ramadhan bisa dipakai nonton 50.220 video kultum ustadz versi HD lebih banyak dari bulan sebelumnya (Berkah ramadhaaaan…)',
+          'Paket internet yang dibeli penghuni Kosan Bukalapak di bulan Ramadhan 2018 bisa dipakai untuk nonton <strong>50.220 video</strong> konten religius dengan kualitas HD. Lebih banyak ya yang bisa ditonton dibanding bulan sebelumnya. Berkah Ramadhaaaan…',
         icon: '/images/ic_paketinternet1.png'
       },
       {
         text:
-          'Para penghuni kosan Bukalapak juga mempersiapkan diri untuk mudik lebaran lho. Anak-anak yang pulang menuhin hampir 169 gerbong kereta.',
+          'Selama bulan puasa, transaksi di Bukalapak bisa menyalakan listrik ke <strong>37.415 rumah</strong> selama sebulan atau hampir <strong>1,5 kali</strong> lebih banyak dari bulan sebelumnya.',
+        icon: '/images/ic_listrikprabayar1.png'
+      },
+      {
+        text:
+          'Para penghuni Kosan Bukalapak juga mempersiapkan diri untuk mudik lebaran lho. Terhitung yang akan berangkat mudik <strong>hampir 169 gerbong kereta.</strong>',
         icon: '/images/ic_kereta1.png'
       },
       {
         text:
-          'Ternyata, destinasi favorit penghuninya adalah Bandung (15 gerbong), dan Cirebon (13 gerbong), Semarang (17 gerbong). Jadi penasaran, kebutuhan rumah tangga di kota destinasi favorit meningkat gak ya karena penduduknya pulang kampung?',
+          'Ternyata ada tiga destinasi yang paling banyak dituju oleh penghuni kosan adalah <strong>Semarang (17 gerbong)</strong>, <strong>Bandung (15 gerbong)</strong>, dan <strong>Cirebon (13 gerbong)</strong>.',
         icon: '/images/ic_kereta1.png'
       },
       {
         text:
-          'Saat penghuni kosan Bukalapak pada mudik, ternyata di kota-kota destinasinya terdapat peningkatan penggunaan air dan listrik ya.',
+          'Jadi penasaran nih, apa kebutuhan rumah tangga di kota destinasi terbanyak juga meningkat ya karena penduduknya pulang kampung? Yuk, kita lihat gimana penggunaan air dan listriknya!',
         icon: '/images/ic_kereta1.png'
       }
     ],
@@ -109,7 +112,8 @@ export default [
         },
         {
           label: '<img src="/images/ic_data_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_data_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_data_disable-min.png" alt="">',
           data: 0.7707193297
         },
         {
@@ -121,71 +125,9 @@ export default [
       ],
       comparisons: [
         {
-          location: 'Semarang',
-          description:
-            'Kalau di Semarang, penggunaan listriknya meningkat 2x lipat juga nih, hmm yang pada pulang kampung pada rebutan ngecas hp apa gimana. Sementara pembayaran airnya meningkat 1.7x lipat…',
-          virtualProducts: [
-            {
-              label: '<img src="/images/ic_air_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_air_disable-min.png" alt="">',
-              data: 0.07499727864
-            },
-            {
-              label: '<img src="/images/ic_pulsa_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_pulsa_disable-min.png" alt="">',
-              data: 0.0423807646
-            },
-            {
-              label: '<img src="/images/ic_data_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_data_disable-min.png" alt="">',
-              data: 0.030412324
-            },
-            {
-              label: '<img src="/images/ic_listrik_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_listrik_disable-min.png" alt="">',
-              data: 0.04068363228
-            }
-          ]
-        },
-        {
-          location: 'Cirebon',
-          description:
-            'Liat ke Cirebon deh, kalo disini ternyata penggunaannya meningkat 1.5x, sedangkan airnya meningkat sampai 4.3x lipat di bulan Ramadhan ini!',
-          virtualProducts: [
-            {
-              label: '<img src="/images/ic_air_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_air_disable-min.png" alt="">',
-              data: 0.0002620895714
-            },
-            {
-              label: '<img src="/images/ic_pulsa_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_pulsa_disable-min.png" alt="">',
-              data: 0.01199393756
-            },
-            {
-              label: '<img src="/images/ic_data_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_data_disable-min.png" alt="">',
-              data: 0.009666154867
-            },
-            {
-              label: '<img src="/images/ic_listrik_enable-min.png" alt="">',
-              labelDisabled:
-                '<img src="/images/ic_listrik_disable-min.png" alt="">',
-              data: 0.00768299301
-            }
-          ]
-        },
-        {
           location: 'Bandung',
           description:
-            'Ternyata, kebutuhan rumah tangga di Bandung meningkat nih penggunaan listriknya saat anak kosan Bukalapak pulang kampung. jumlahnya mencapai 2x dari bulan sebelumnya. Pemakaian air juga meningkat 1.3x dibanding bulan sebelumnya.',
+            'Kebutuhan listrik di kota <strong>Bandung</strong> meningkat penggunaannya saat anak kosan Bukalapak mudik. Dari pencatatan sih, jumlahnya naik sampai <strong>2 kali</strong> dari bulan sebelumnya. Penggunaan air juga meningkat hampir <strong>1,5 kali</strong> dibanding bulan sebelumnya. ',
           virtualProducts: [
             {
               label: '<img src="/images/ic_air_enable-min.png" alt="">',
@@ -206,10 +148,75 @@ export default [
               data: 0.206912903
             },
             {
-              label: '<img src="/images/ic_listrik_enable-min.png" alt="">',
+              label:
+                '<img src="/images/ic_listrik_enable-min.png" alt="">',
               labelDisabled:
                 '<img src="/images/ic_listrik_disable-min.png" alt="">',
               data: 0.08789060475
+            }
+          ]
+        },
+        {
+          location: 'Semarang',
+          description:
+            'Kalau di <strong>Semarang</strong>, penggunaan listriknya meningkat <strong>2 kali</strong> lipat juga nih. Sedangkan, pembayaran airnya lebih dari <strong>1,5 kali</strong> lipat. Hmm apa yang pulang kampung pada rebutan ngecharge handphone ya?',
+          virtualProducts: [
+            {
+              label: '<img src="/images/ic_air_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_air_disable-min.png" alt="">',
+              data: 0.07499727864
+            },
+            {
+              label: '<img src="/images/ic_pulsa_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_pulsa_disable-min.png" alt="">',
+              data: 0.0423807646
+            },
+            {
+              label: '<img src="/images/ic_data_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_data_disable-min.png" alt="">',
+              data: 0.030412324
+            },
+            {
+              label:
+                '<img src="/images/ic_listrik_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_listrik_disable-min.png" alt="">',
+              data: 0.04068363228
+            }
+          ]
+        },
+        {
+          location: 'Cirebon',
+          description:
+            'Kalau kita liat ke <strong>Cirebon</strong>, kita juga liat ada kenaikan penggunaan di dua jenis kebutuhan rumah tangga. Untuk penggunaan listrik ternyata meningkat sampai <strong>1,5 kali</strong>, sedangkan buat penggunaan airnya meningkat sampai lebih dari <strong>4 kali lipat</strong> di bulan Ramadhan ini!',
+          virtualProducts: [
+            {
+              label: '<img src="/images/ic_air_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_air_disable-min.png" alt="">',
+              data: 0.0002620895714
+            },
+            {
+              label: '<img src="/images/ic_pulsa_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_pulsa_disable-min.png" alt="">',
+              data: 0.01199393756
+            },
+            {
+              label: '<img src="/images/ic_data_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_data_disable-min.png" alt="">',
+              data: 0.009666154867
+            },
+            {
+              label:
+                '<img src="/images/ic_listrik_enable-min.png" alt="">',
+              labelDisabled:
+                '<img src="/images/ic_listrik_disable-min.png" alt="">',
+              data: 0.00768299301
             }
           ]
         }
@@ -230,11 +237,6 @@ export default [
       },
       {
         text:
-          'Ternyata, setelah ada tambahan penghuni baru, gak semua penggunaan kebutuhan rumah tangga meningkat. Buat listrik, penggunaannya sih memang meningkat 1.8x kali dari bulan sebelumnya.',
-        icon: '/images/ic_listrikprabayar1.png'
-      },
-      {
-        text:
           'Tapi, buat penggunaan air jadi 1.2x lebih rendah dari bulan Ramadhan. Semoga sih bukan karena pada gak wudhu lagi ya.',
         icon: '/images/ic_pdam1.png'
       },
@@ -247,23 +249,31 @@ export default [
         text:
           'Penghuni di kosan Bukalapak kelihatannya makin sering nonton video lho setelah balik mudik. Soalnya, transaksi paket datanya meningkat sebanyak 1.2x.',
         icon: '/images/ic_paketinternet1.png'
-      }
+      },
+      {
+        text:
+          'Ternyata, setelah ada tambahan penghuni baru, gak semua penggunaan kebutuhan rumah tangga meningkat. Buat listrik, penggunaannya sih memang meningkat 1.8x kali dari bulan sebelumnya.',
+        icon: '/images/ic_listrikprabayar1.png'
+      },
     ],
     transactions: {
       virtualProducts: [
         {
           label: '<img src="/images/ic_air_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_air_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_air_disable-min.png" alt="">',
           data: 0.8165389151
         },
         {
           label: '<img src="/images/ic_pulsa_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_pulsa_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_pulsa_disable-min.png" alt="">',
           data: 1
         },
         {
           label: '<img src="/images/ic_data_enable-min.png" alt="">',
-          labelDisabled: '<img src="/images/ic_data_disable-min.png" alt="">',
+          labelDisabled:
+            '<img src="/images/ic_data_disable-min.png" alt="">',
           data: 1
         },
         {
@@ -276,27 +286,27 @@ export default [
       comparisons: [
         {
           location: 'Jogja',
-          description: 'Semarang description',
+          description: 'Jogja description',
           thumbnailUrl: '/images/ill_jogja.png',
           virtualProducts: [
             {
               label: '<img src="/images/ic_kereta_enable.png" alt="">',
               labelDisabled:
                 '<img src="/images/ic_air_disable-min.png" alt="">',
-              data: 0.1287195804
+              data: 0.75
             }
           ]
         },
         {
           location: 'Semarang',
-          description: 'Cirebon description',
+          description: 'Semarang description',
           thumbnailUrl: '/images/ill_semarang.png',
           virtualProducts: [
             {
               label: '<img src="/images/ic_kereta_enable.png" alt="">',
               labelDisabled:
                 '<img src="/images/ic_air_disable-min.png" alt="">',
-              data: 0.001136853579
+              data: 0.75
             }
           ]
         },
@@ -309,7 +319,7 @@ export default [
               label: '<img src="/images/ic_kereta_enable.png" alt="">',
               labelDisabled:
                 '<img src="/images/ic_air_disable-min.png" alt="">',
-              data: 0.1666772105
+              data: 0.6
             }
           ]
         }
